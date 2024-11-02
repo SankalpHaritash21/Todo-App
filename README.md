4de018bee5422afe198bbdeb81267ef34ba8236f
Sankalp <sanklpkaushik21@gmail.com>
2024-11-02 14:20:02 +0530

    Todo App React Native

---
+++ README.md
@@ -0,0 +1,7 @@
+# Todo App
+
+This is a simple Todo app built with React Native, utilizing expo-router, FlatList for efficient rendering, and MaterialCommunityIcons for icons. The app enables users to add, complete, and delete tasks.
+
+![Todo React Native App](./todos.jfif)
+
+Enjoy building and expanding this simple yet powerful Todo App!
---
+++ app/index.jsx
@@ -0,0 +1,137 @@
+import {
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+import { data } from "@/data/todo";
+
+export default function Index() {
+  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+  const [text, setText] = useState("");
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{ id: newId, title: text, completed: false }, ...todos]);
+      setText("");
+    }
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={style.todoItem}>
+      <Text
+        style={[style.todoText, item.completed && style.completedText]}
+        onPress={() => toggleTodo(item.id)}
+      >
+        {item.title}
+      </Text>
+      <Pressable onPress={() => removeTodo(item.id)}>
+        <MaterialCommunityIcons
+          name="delete-circle"
+          size={36}
+          color="red"
+          selectable={undefined}
+        />
+      </Pressable>
+    </View>
+  );
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  return (
+    <SafeAreaView style={style.container}>
+      <View style={style.inputContainer}>
+        <TextInput
+          style={style.input}
+          placeholder="Add a new todo"
+          placeholderTextColor="gray"
+          value={text}
+          onChangeText={setText}
+        />
+        <Pressable onPress={addTodo} style={style.addButton}>
+          <Text style={style.addButtonText}>Add</Text>
+        </Pressable>
+      </View>
+      <FlatList
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={(todo) => todo.id}
+        contentContainerStyle={{ flexGrow: 1 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    padding: 10,
+    width: "100%",
+    maxWidth: 1024,
+    marginHorizontal: "auto",
+    pointerEvents: "auto",
+  },
+  input: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    fontSize: 18,
+    minWidth: 0,
+    color: "white",
+  },
+  addButton: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    padding: 10,
+  },
+  addButtonText: {
+    fontSize: 18,
+    color: "black",
+  },
+  todoItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 4,
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    width: "100%",
+    maxWidth: 1024,
+    marginHorizontal: "auto",
+    pointerEvents: "auto",
+  },
+  todoText: {
+    flex: 1,
+    fontSize: 18,
+    color: "white",
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    color: "gray",
+  },
+});
